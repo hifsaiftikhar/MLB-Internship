bf761e54ad81a765e499cdedd6274c87e6e362e6
hifsaiftikhar <hifsaiftikhar20@gmail.com>
2026-09-12 14:57:01 +0500

    Day-30: config

---
+++ Day-30/app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = "your_fallback_secret_key_change_in_production"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8-sig",
+        extra="ignore",
+        case_sensitive=False
+    )
+
+settings = Settings()
